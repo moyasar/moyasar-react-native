9746f807fab257ea9c94264f557daf2e317170b0
Abdulaziz Alrabiah <afalrabiah98@gmail.com>
2025-05-03 19:46:22 +0300

    docs: fix currency code format typo

--- src/models/api/api_requests/payment_request.ts
+++ src/models/api/api_requests/payment_request.ts
@@ -3,7 +3,7 @@ import type { PaymentRequestSource } from '../sources/payment_request_source';
 /**
  * Constructs a PaymentRequest object for creating a payment.
  * @param {number} amount - The amount to be charged in the smallest currency unit. For example, to charge `SAR 257.58` you will have the [amount] as `25758`. In other words, 10 SAR = 10 * 100 Halalas. Integer values only.
- * @param {string} [currency='SAR'] - The currency code for the payment. Defaults to 'SAR'. Must be in ISO 3166-1 alpha-3 country code format.
+ * @param {string} [currency='SAR'] - The currency code for the payment. Defaults to 'SAR'. Must be in ISO 4217 3-letter currency code format.
  * @param {string | null} [description] - Can be any string you want to tag the payment. For example `Payment for Order #34321`.
  * @param {Record<string, string | number | boolean> | null} [metadata] - Adds searchable key/value pairs to the payment. For example `{"size": "xl"}`.
  * @param {PaymentRequestSource} source - A payment source object to be charged, such as Apple Pay source or Credit Card source.
