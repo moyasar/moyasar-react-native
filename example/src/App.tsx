d230a707233de24e464fdc270b1ed642d7ceb41f
Sultan Algarbi <sultanye8@gmail.com>
2026-05-18 13:01:57 +0300

    feat: support inline styles for Samsung Pay button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -113,10 +113,7 @@ export default function App() {
         <SamsungPay
           paymentConfig={paymentConfig}
           onPaymentResult={onPaymentResult}
-          style={{
-            width: '90%',
-            buttonType: SamsungPayButtonType.payWithSamsungPay,
-          }}
+          style={samsungPayStyle}
         />
         <CreditCard
           paymentConfig={paymentConfig}
@@ -126,12 +123,19 @@ export default function App() {
         <ApplePay
           paymentConfig={paymentConfig}
           onPaymentResult={onPaymentResult}
-          style={{ buttonType: 'buy' }}
+          style={applePayStyle}
         />
       </View>
     </ScrollView>
   );
 }
+
+const samsungPayStyle = {
+  width: '90%' as const,
+  buttonType: SamsungPayButtonType.payWithSamsungPay,
+};
+
+const applePayStyle = { buttonType: 'buy' as const };
 
 const styles = StyleSheet.create({
   container: {
